b2dced61dadbd8298789374f7cd9e4dc4640973f
Vertu User <vertu20140207@gmail.com>
2017-05-30 13:11:52 +0100

    Add tests
    Signed-off-by: Vertu User <vertu20140207@gmail.com>

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-SRCDIR=/usr/local/tomcat/test-data
-mkdir $SRCDIR \
+SRCDIR=/usr/local/tomcat/test-data/test-repo
+mkdir -p $SRCDIR \
   && cd $SRCDIR \
   && git init \
   && cat <<END > main.c
@@ -9,10 +9,14 @@ int main(int argc, char** argv) {
     printf("Hello\n");
 }
 END
-git add main.c \
+git config --global user.email "test@localhost" \
+  && git config --global user.name "Test" \
+  && git add main.c \
   && git commit -m"Test source"
-opengrok-*/bin/OpenGrok deploy \
-    && opengrok-*/bin/OpenGrok index $SRCDIR \
+cd /usr/local/tomcat
+/usr/local/tomcat/${OPENGROKVERSION}/bin/OpenGrok deploy \
+    && /usr/local/tomcat/${OPENGROKVERSION}/bin/OpenGrok index $(dirname $SRCDIR) \
     && sudo service cron start \
-    && catalina.sh run
-curl -f http://localhost:8080/source/xref/$(basename $SRCDIR)
+    && catalina.sh start
+sleep 10
+curl -f http://localhost:8080/source/xref/$(basename $SRCDIR)/
